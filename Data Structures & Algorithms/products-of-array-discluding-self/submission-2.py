cc7e54e0dedb762440e1e3c548865c70771d510b
Micah Yong <micahtyong@gmail.com>
2026-05-20 09:26:08 -0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,38 @@
+# Naive: O(n^2) if you recompute each time
+# (n - 1) + (n - 1) ... // n times for each element
+# Better: n - 1 + n => O(n) if you shift / divide / multiply
+#   - But how would you handle the division by 0 case?
+# Follow-up
+
+# Prefix-sum? Prefix-mult? 
+# [1, 2, 8, 48]
+
+# Left to right AND right to left
+# [1, 2, 4, 6]
+#   => 1, 1, 2, 8
+#   => 48, 24, 6, 1
+# Then multiply
+
+# [-1, 0, 1, 2, 3]
+#   => 1, -1, 0, 0, 0
+#   => 0, 6, 6, 3, 1
+
+class Solution:
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        nums_reversed = nums[::-1]
+        l2r, r2l = [1], [1]
+        # Populate l2r
+        for i in range(0, len(nums) - 1):
+            l2r.append(l2r[-1] * nums[i])
+            r2l.append(r2l[-1] * nums_reversed[i])
+        r2l = r2l[::-1]
+  
+        print(l2r, r2l)
+        res = []
+        for i in range(len(nums)):
+            res.append(l2r[i] * r2l[i])
+        print(res)
+        return res
+
+
