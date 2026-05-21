a1079f4616942805cabc10d0ba653f8bc591a079
Micah Yong <micahtyong@gmail.com>
2026-05-20 18:03:50 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,9 @@
+# Naive: Double-for loop, O(n^2)
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(len(prices) - 1):
+            for j in range(i, len(prices)):
+                profit = max(profit, prices[j] - prices[i])
+        return profit
