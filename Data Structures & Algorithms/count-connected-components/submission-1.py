736d8ebad403005b4fc4bef2073f041330e9b830
Micah Yong <micahtyong@gmail.com>
2026-05-26 09:14:27 -0700

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.py
@@ -0,0 +1,56 @@
+# Union-find with path compression.
+# Data structures
+#   - num_connected initialized to n
+#   - parents list initialized to [i for i in range(n)]
+#   - [opt] size list initialized to [1 for each elem in edges]
+# Helper methods
+#   - find(x)
+#       - find the root
+#       - set any intermediaries to that root
+#   - union(i,j)
+#       - find roots of i and j
+#       - if equal, return
+#       - otherwise, set smaller root to parent
+# For each union, if they are not already part of the same component, num_connected -= 1
+
+# Edge cases
+#   - Cycles: Won't be represented as a cycle in our data struct.
+#       - Split happens and edges go to the parent representative
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        num_connected = n
+        parent = [i for i in range(n)]
+        sizes = [1 for _ in range(n)]
+        
+        def find(x) -> int: 
+            # Find root
+            root = x
+            while parent[root] != root:
+                root = parent[root]
+             
+            # Path compression
+            ptr = x
+            while parent[ptr] != root:
+                temp = ptr
+                ptr = parent[ptr]
+                parent[temp] = root
+
+            return root
+
+        def union(i, j) -> None:
+            nonlocal num_connected
+            root_i = find(i)
+            root_j = find(j)
+            if root_i == root_j: return
+            parent[root_i] = root_j
+            num_connected -= 1
+            # TODO: Size optimization
+
+        for edge in edges:
+            i, j = edge
+            union(i, j)
+            # print(parent, num_connected)
+
+        return num_connected
+        
